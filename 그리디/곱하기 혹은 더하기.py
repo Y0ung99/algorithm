6362f915946b0d5813a9dfd58c0676089af15bb0
KUM <ancolor@knou.ac.kr>
2023-08-08 23:54:58 +0900

    곱하기 혹은 더하기 다시 품

--- 그리디/곱하기 혹은 더하기.py
+++ 그리디/곱하기 혹은 더하기.py
@@ -1,14 +1,28 @@
-#1
+#1 -> 1을 곱하는 경우를 생각하지 못함
+# s = input()
+# answer = 0
+
+# for c in s:
+#     n = int(c)
+#     if 0 < n:
+#         if answer == 0:
+#             answer = 1
+#         answer *= n
+#     else:
+#         answer += n
+
+# print(answer)
+
+#2
 s = input()
 answer = 0
 
 for c in s:
     n = int(c)
-    if 0 < n:
-        if answer == 0:
-            answer = 1
-        answer *= n
-    else:
+    if answer == 0 or n == 0 or answer == 1 or n == 1:
         answer += n
+    else:
+        answer *= n
+
 
 print(answer)
