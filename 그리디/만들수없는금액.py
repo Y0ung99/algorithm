5af466519cb53958243f55f645264c221fb777ad
KUM <ancolor@knou.ac.kr>
2023-08-13 00:26:58 +0900

    그리디 실전 4, 5, 6문제 책풀이 추가

--- 그리디/만들수없는금액.py
+++ 그리디/만들수없는금액.py
@@ -1,18 +1,36 @@
 # 하나씩 더할때, 두개씩 더할때, n개씩 더할때 금액들 세트에 추가
 # 하나씩 탐색하며 제일 작은 숫자 찾기
 # 1
-from itertools import combinations
+# from itertools import combinations
 
+# n = int(input())
+# arr = list(map(int, input().split()))
+# sets = set()
+
+# for i in range(1, n+1, 1):
+#     cases = list(combinations(arr, i))
+#     for s in cases:
+#         sets.add(sum(s))
+
+# for index, e in enumerate(sets):
+#     if index+1 != e:
+#         print(index+1)
+#         break
+
+# 책풀이
+'''
+#1은 애초에 그리디가 아님.
+그리디 -> 현재 상태에서 매번 가장 좋아 보이는 것만을 선택하는 알고리즘
+'''
 n = int(input())
-arr = list(map(int, input().split()))
-sets = set()
+data = list(map(int, input().split))
+data.sort()
 
-for i in range(1, n+1, 1):
-    cases = list(combinations(arr, i))
-    for s in cases:
-        sets.add(sum(s))
-
-for index, e in enumerate(sets):
-    if index+1 != e:
-        print(index+1)
+target = 1
+for x in data:
+    #만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
         break
+    target += x
+
+print(target)
--- 그리디/볼링공고르기.py
+++ 그리디/볼링공고르기.py
@@ -1,15 +1,34 @@
-from itertools import combinations
+# from itertools import combinations
 
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# arr = list(combinations(arr, 2))
+# remove_set = set()
+
+# for e in arr:
+#     if e[0] == e[1]:
+#         remove_set.add(e)
+
+# result = [i for i in arr if i not in remove_set]
+
+# print(len(result))
+
+#책풀이
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+data = list(map(int, input().split()))
 
-arr = list(combinations(arr, 2))
-remove_set = set()
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+array =  [0] * 11
 
-for e in arr:
-    if e[0] == e[1]:
-        remove_set.add(e)
+for x in data:
+    #각 무게에 해당하는 볼링공의 개수 카운트
+    array[x] += 1
 
-result = [i for i in arr if i not in remove_set]
+result = 0
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m + 1):
+    n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+    result += array[i] * n # B가 선택하는 경우의 수와 곱하기
 
-print(len(result))
+print(result)
